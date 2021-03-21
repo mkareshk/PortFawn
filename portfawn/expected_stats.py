21a2ce2e9986fd2db11ed9d0127ab78dd616997c
Moeion Kareshk <moein.owhadi@gmail.com>
2021-03-21 10:52:42 -0700

    add docs to the expected_stats

--- portfawn/expected_stats.py
+++ portfawn/expected_stats.py
@@ -1,50 +1,60 @@
+import time
+
 import numpy as np
 
 
 class ExpectedStats:
-    def __init__(self, returns, optimization_type):
+    def __init__(self, returns_data, optimization_type):
 
-        self.returns = returns
+        self.returns_data = returns_data
         self.optimization_type = optimization_type
 
-    def _get_sample_set(self, sample_type):
+    def _get_sample_set(self, sample_type, random_selection_ratio):
 
         if sample_type == "full":
-            return self.returns.values
+            return self.returns_data.values
 
         elif sample_type == "random":
             selected_rows = np.random.randint(
-                low=0, high=len(self.returns), size=int(len(self.returns) / 5)
+                low=0,
+                high=len(self.returns_data),
+                size=int(len(self.returns_data * random_selection_ratio)),
             )
-            return self.returns.values[selected_rows, :]
+            return self.returns_data.values[selected_rows, :]
 
         elif sample_type == "weighted_tail":
-            p = np.linspace(start=0.01, stop=1.0, num=len(self.returns))
+            p = np.linspace(start=0.01, stop=1.0, num=len(self.returns_data))
             p = p / sum(p)
             selected_rows = np.random.choice(
-                range(len(self.returns)), size=len(self.returns), p=p
+                range(len(self.returns_data)), size=len(self.returns_data), p=p
             )
-            return self.returns.values[selected_rows, :]
+            return self.returns_data.values[selected_rows, :]
 
-    def expected_mean_cov(self, sample_type, instance_num=10):
+    def expected_mean_cov(
+        self, sample_type, instance_num=10, random_selection_ratio=0.2
+    ):
 
         if sample_type == "full":
-            sample = self._get_sample_set(sample_type)
-            mean, cov = np.mean(sample, axis=0), np.cov(sample.T)
+            sample_set = self._get_sample_set(sample_type, random_selection_ratio)
 
         elif sample_type in ["random", "weighted_tail"]:
             instance_num = np.max(5, instance_num)
             sample_set = []
             for i in range(instance_num):
                 np.random.seed(int(np.sqrt(time.time()) * i))
-                sample_set.append(self._get_sample_set(sample_type))
+                sample = self._get_sample_set(sample_type, random_selection_ratio)
+                sample_set.append(sample)
             sample_set = np.array(sample_set[0])
-            mean, cov = np.mean(sample_set, axis=0), np.cov(sample_set.T)
 
-        if self.optimization_type in ["binary_sa", "binary_qpu"]:
-            mean = (mean - mean.min()) / (
-                mean.max() - mean.min()
-            )  # scale normalization
+        mean, cov = np.mean(sample_set, axis=0), np.cov(sample_set.T)
+
+        if self.optimization_type in [
+            "binary_sa",
+            "binary_qpu",
+        ]:  # convert the stats to QUBO-ready ones
+            mi = mean.min()
+            ma = mean.max()
+            mean = (mean - mi) / (ma - mi)  # scale normalization
             cov = (cov - cov.mean()) / cov.std()  # z-score normalization
             mean, cov = np.rint(mean), np.rint(cov)
 
