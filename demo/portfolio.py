c8092c5a7e4fa96074ef21522d35d0f25adbfb16
Moein Kareshk <mkareshk@outlook.com>
2024-02-17 11:16:25 -0800

    update the demo

--- demo/portfolio.py
+++ demo/portfolio.py
@@ -1,28 +1,68 @@
 # get data
 import dafin
 
-data_instance = dafin.ReturnsData(["AAPL", "GOOGL"])
-returns_data = data_instance.get_returns("2022-01-01", "2022-12-30")
-print(returns_data)
+asset_lsit = ["SPY", "GLD", "BND"]
+date_start = "2010-01-01"
+date_end = "2022-12-30"
 
+data_instance = dafin.ReturnsData(asset_lsit)
+returns_data = data_instance.get_returns(date_start, date_end)
 
 # portfolio
-import portfawn
+from portfawn import (
+    BackTest,
+    RandomPortfolio,
+    EquallyWeightedPortfolio,
+    MeanVariancePortfolio,
+)
 
-# random portfolio
-random_portfolio = portfawn.portfolio.RandomPortfolio()
-random_portfolio.fit(returns_data)
-returns_portfolio = random_portfolio.evaluate(returns_data)
-print(returns_portfolio)
+portfolio_list = [
+    RandomPortfolio(),
+    EquallyWeightedPortfolio(),
+    MeanVariancePortfolio(),
+]
 
-# equally weighted
-equally_weighted_portfolio = portfawn.portfolio.EquallyWeightedPortfolio()
-equally_weighted_portfolio.fit(returns_data)
-returns_portfolio = equally_weighted_portfolio.evaluate(returns_data)
-print(returns_portfolio)
+backtest = BackTest(
+    portfolio_list=portfolio_list,
+    asset_list=asset_lsit,
+    date_start=date_start,
+    date_end=date_end,
+    fitting_days=252,
+    evaluation_days=22,
+    n_jobs=12,
+)
+backtest.run()
 
-# mean variance
-mean_var_portfolio = portfawn.portfolio.MeanVariancePortfolio()
-mean_var_portfolio.fit(returns_data)
-returns_portfolio = mean_var_portfolio.evaluate(returns_data)
-print(returns_portfolio)
+
+import matplotlib.pyplot as plt
+
+fig, ax = backtest.plot_returns()
+plt.savefig("random_portfolio_returns.png")
+
+fig, ax = backtest.plot_cum_returns()
+plt.savefig("random_portfolio_cum_returns.png")
+
+
+fig, ax = backtest.plot_dist_returns()
+plt.savefig("random_portfolio_dist_returns.png")
+
+
+fix, ax = backtest.plot_corr()
+plt.savefig("random_portfolio_corr.png")
+
+# plot_cov
+fig, ax = backtest.plot_cov()
+plt.savefig("random_portfolio_cov.png")
+
+
+# # equally weighted
+# equally_weighted_portfolio = portfawn.portfolio.EquallyWeightedPortfolio()
+# equally_weighted_portfolio.fit(returns_data)
+# performance = equally_weighted_portfolio.evaluate(returns_data)
+# print(performance)
+
+# # mean variance
+# mean_var_portfolio = portfawn.portfolio.MeanVariancePortfolio()
+# mean_var_portfolio.fit(returns_data)
+# performance = mean_var_portfolio.evaluate(returns_data)
+# print(performance)
