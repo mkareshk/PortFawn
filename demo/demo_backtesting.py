5aac376aa7c1186e24f7283d44e7d0a3356ea782
Moein Kareshk <mkareshk@outlook.com>
2024-03-04 09:54:46 -0800

    refactor and enhance the demos for portfolio and backtesting

---
+++ demo/demo_backtesting.py
@@ -0,0 +1,50 @@
+import dafin
+import matplotlib.pyplot as plt
+
+from portfawn import (
+    BackTest,
+    RandomPortfolio,
+    EquallyWeightedPortfolio,
+    MeanVariancePortfolio,
+)
+
+# params
+asset_lsit = ["SPY", "GLD", "BND"]
+date_start = "2010-01-01"
+date_end = "2022-12-30"
+data_instance = dafin.ReturnsData(asset_lsit)
+returns_data = data_instance.get_returns(date_start, date_end)
+
+portfolio_list = [
+    RandomPortfolio(),
+    EquallyWeightedPortfolio(),
+    MeanVariancePortfolio(),
+]
+
+# backtesting
+backtest = BackTest(
+    portfolio_list=portfolio_list,
+    asset_list=asset_lsit,
+    date_start=date_start,
+    date_end=date_end,
+    fitting_days=252,
+    evaluation_days=252,
+    n_jobs=12,
+)
+backtest.run()
+
+# visualization
+fig, ax = backtest.plot_returns()
+plt.savefig("plot_returns.png")
+
+fig, ax = backtest.plot_cum_returns()
+plt.savefig("plot_cum_returns.png")
+
+fig, ax = backtest.plot_dist_returns()
+plt.savefig("plot_dist_returns.png")
+
+fix, ax = backtest.plot_corr()
+plt.savefig("plot_corr.png")
+
+fig, ax = backtest.plot_cov()
+plt.savefig("plot_cov.png")
---
+++ demo/demo_portfolio.py
@@ -0,0 +1,27 @@
+import dafin
+
+from portfawn import (
+    RandomPortfolio,
+    EquallyWeightedPortfolio,
+    MeanVariancePortfolio,
+)
+
+# params
+assets_list = ["SPY", "GLD", "BND"]
+date_start = "2010-01-01"
+date_end = "2022-12-30"
+
+data_instance = dafin.ReturnsData(assets_list)
+returns_data = data_instance.get_returns(date_start, date_end)
+
+# portfolio optimization
+portfolio_list = [
+    RandomPortfolio(),
+    EquallyWeightedPortfolio(),
+    MeanVariancePortfolio(),
+]
+
+for portfolio in portfolio_list:
+    portfolio.fit(returns_data)
+    performance = portfolio.evaluate(returns_data)
+    print(portfolio)
